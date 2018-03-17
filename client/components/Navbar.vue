2f8395f9643828cd6276bc88acd09f4c9d663455
kiyon <kiyonlin@163.com>
2018-03-17 09:15:51 +0800

    refactor nav bar

---
+++ client/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <b-container fluid class="pl-0 pr-0">
+      <b-navbar toggleable="sm" type="light" variant="light" class="px-md-2 px-lg-5" fixed="top">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand :to="$i18n.path('')" exact class="ml-lg-5">{{appName}}</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse" class="mr-lg-5">
+          <b-navbar-nav>
+            <b-nav-item :to="$i18n.path('')" exact>{{ $t('links.home') }}</b-nav-item>
+            <b-nav-item :to="$i18n.path('about')" exact>{{ $t('links.about') }}</b-nav-item>
+            <b-nav-item :to="$i18n.path('invitation')" exact>{{ $t('links.invitation') }}</b-nav-item>
+          </b-navbar-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" type="text" :placeholder="$t('links.search')" />
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">{{ $t('links.search') }}</b-button>
+            </b-nav-form>
+            <b-nav-item-dropdown :text="$t('links.lang')" right>
+              <b-dropdown-item :to="$i18n.locale === 'en' ? $route.fullPath : `/en` + $route.fullPath" exact>
+                {{ $t('links.english') }}
+              </b-dropdown-item>
+              <b-dropdown-item :to="$i18n.locale === 'zh_cn' ? $route.fullPath : $route.fullPath.replace(/^\/[^\/]+/, '')" exact>
+                {{ $t('links.chinese') }}
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+          <b-navbar-nav>
+            <!-- Authenticated -->
+            <b-nav-item-dropdown right v-if="true">
+              <template slot="button-content">
+                <em>{{ $t('links.memberCenter') }}</em>
+              </template>
+              <b-dropdown-item :to="$i18n.path('setting/profile')">{{ $t('links.profile') }}</b-dropdown-item>
+              <b-dropdown-item @click.prevent="logout">{{ $t('links.logout') }}</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <!-- Guest -->
+            <template v-if="!user">
+              <b-nav-item :to="$i18n.path('auth/login')" exact>{{ $t('links.login') }}</b-nav-item>
+              <b-btn :to="$i18n.path('auth/register')" variant="outline-success">{{ $t('links.register') }}</b-btn>
+            </template>
+          </b-navbar-nav>          
+        </b-collapse>
+      </b-navbar>
+    </b-container>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data: () => ({
+    appName: process.env.appName
+  }),
+
+  computed: mapGetters({
+    user: "auth/user"
+  }),
+
+  methods: {
+    async logout() {
+      // Log out the user.
+      await this.$store.dispatch("auth/logout");
+      // Redirect to login.
+      this.$router.push({ name: "login" });
+    }
+  }
+};
+</script>
+<style>
+
+</style>
