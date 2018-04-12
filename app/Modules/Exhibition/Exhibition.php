9da51634f35a11dfaa1a34e5118a7229e3373e15
kiyon <kiyonlin@163.com>
2018-04-12 15:48:04 +0800

    init exhibition module

---
+++ app/Modules/Exhibition/Exhibition.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Exhibition;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exhibition extends Model
+{
+    //
+}
---
+++ app/Modules/Exhibition/ExhibitionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Exhibition;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExhibitionController extends Controller
+{
+    //
+}
